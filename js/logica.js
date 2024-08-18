0c90af90a49c0d4ec2c6f8de4df58761b9a84956
meca-zamorano <eimycabrerazamorano@gmail.com>
2024-08-18 12:08:56 -0500

    Se agrega la logica del boton encriptar

--- js/logica.js
+++ js/logica.js
@@ -1,7 +1,28 @@
-const textInput = document.getElementById('textInput');
-const messageContainer = document.getElementById('message-container');
+const textArea = document.getElementById('textarea');
+const messageContainer = document.getElementById('main-msm');
+const abecedario = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
-textInput.addEventListener('input', function() {
+//FUNCION PARA GENERAR UNA LETRA ALEATORIA DEL ABECEDARIO 
+function letraAleatoria() {
+    const letra = Math.floor(Math.random() * abecedario.length);
+    return abecedario[letra];
+}
+const letraA = letraAleatoria();
+console.log(letraA);
+
+//FUNCION PARA ENCRIPTAR, ESTE INSERTA LA LETRA ALEATORIA EN EL TEXTO DIGITADO POR EL USUARIO
+function encriptar(textArea){
+    let textoEncriptado ='';
+    for(let i = 0; i<textArea.length; i++ ){
+        let caracter = textArea[i];
+        if (caracter >= 'a' && caracter <= 'z') {
+            textoEncriptado += caracter;
+            textoEncriptado += obtenerLetraAleatoria();
+    }
+    return textoEncriptado;
+}
+
+textArea.addEventListener('input', function() {
     if (this.value.trim() !== "") {
         messageContainer.style.opacity = 0;
     } else {
